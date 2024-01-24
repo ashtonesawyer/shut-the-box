666b94011aae9ea6e43e0a584b7114fde23c6e23
ashtonesawyer <ashtonesawyer@gmail.com>
2024-01-23 18:11:58 -0800

    font awesome + format

--- Dice.js
+++ Dice.js
@@ -1,51 +1,50 @@
 class Dice {
-    faces = [];
+  faces = [];
 
-    constructor() {
-        this.#createFaces();
+  constructor() {
+    this.#createFaces();
+  }
+
+  roll() {
+    // return [0, 6)
+    return Math.floor(Math.random() * 6);
+  }
+
+  #createFaces() {
+    // create elements
+    let faces = [];
+    for (let i = 0; i < 6; i++) {
+      let face = [];
+      for (let j = 0; j < 9; j++) {
+        // maybe replace period with font awesome circle
+        // -- would take some refactoring to do but might look nicer
+        let point = document.createElement("i");
+        point.setAttribute("class", "fa-solid fa-circle fa-2xs die-point off");
+        // point.textContent = ".";
+        face.push(point);
+      }
+      faces.push(face);
     }
 
-    roll() {
-        // return [0, 6)
-        return Math.floor(Math.random() * 6);
-    }
-
-    #createFaces() {
-        // create elements
-        let faces = [];
-        for (let i = 0; i < 6; i++) {
-            let face = [];
-            for (let j = 0; j < 9; j++) {
-                // maybe replace period with font awesome circle
-                // -- would take some refactoring to do but might look nicer
-                let point = document.createElement("div");
-                point.setAttribute("class", "die-point off");
-                point.textContent = ".";
-                face.push(point);
-            }
-            faces.push(face);
+    // set elements
+    let pointIndex = [
+      [4], // 1
+      [2, 6], // 2
+      [2, 4, 6], // 3
+      [0, 2, 6, 8], // 4
+      [0, 2, 4, 6, 8], // 5
+      [0, 2, 3, 5, 6, 8], // 6
+    ];
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (pointIndex[i].includes(j)) {
+          if (faces[i][j].classList.contains("off"))
+            faces[i][j].classList.remove("off");
+          faces[i][j].classList.add("on");
         }
-
-        // set elements
-        let pointIndex = [
-            [4],                // 1 
-            [2, 6],             // 2
-            [2, 4, 6],          // 3
-            [0, 2, 6, 8],       // 4
-            [0, 2, 4, 6, 8],    // 5
-            [0, 2, 3, 5, 6, 8]  // 6
-        ]
-        for (let i = 0; i < 6; i++) {
-            for (let j = 0; j < 9; j++) {
-                if (pointIndex[i].includes(j)) {
-                    if (faces[i][j].classList.contains("off")) 
-                        faces[i][j].classList.remove("off");
-                    faces[i][j].classList.add("on");
-                }
-            }
-        }
-        
-        this.faces = [...faces];
+      }
     }
 
+    this.faces = [...faces];
+  }
 }
--- Main.js
+++ Main.js
@@ -4,136 +4,133 @@ const die2 = document.querySelector("#die2");
 const roll = document.querySelector("#roll");
 const end = document.querySelector("#end");
 const again = document.querySelector("#again");
-const overlay = document.querySelector('.overlay');
+const overlay = document.querySelector(".overlay");
 
 let dice1 = new Dice();
 let dice2 = new Dice();
 let num = 0;
 
 roll.addEventListener("click", () => {
-    // need two instances of Dice() so that they aren't sharing faces in
-    // case result1 == result2
-    let result1 = dice1.roll();
-    let result2 = dice2.roll();
-    const test = [10, 10, 10, 10, 5]
+  // need two instances of Dice() so that they aren't sharing faces in
+  // case result1 == result2
+  let result1 = dice1.roll();
+  let result2 = dice2.roll();
+  const test = [10, 10, 10, 10, 5];
 
-    console.log(result1 +1, result2 +1);
+  console.log(result1 + 1, result2 + 1);
 
-    num = result1 + result2 + 2;
+  num = result1 + result2 + 2;
 
-    die1.replaceChildren(...dice1.faces[result1]);
-    die2.replaceChildren(...dice2.faces[result2]);
+  die1.replaceChildren(...dice1.faces[result1]);
+  die2.replaceChildren(...dice2.faces[result2]);
 
-    // force tile clicks
-    roll.disabled = true;
+  // force tile clicks
+  roll.disabled = true;
 
-    renderTiles();
-
+  renderTiles();
 });
 
-
-box.forEach((tile) => tile.addEventListener("click", () => {
+box.forEach((tile) =>
+  tile.addEventListener("click", () => {
     num -= tile.id;
     tile.classList.add("flipped");
     tile.disabled = true;
 
     if (num === 0) {
-        box.forEach((tile) => {
-            if (!tile.classList.contains("flipped"))
-                tile.disabled = true;
-        })
-        checkWin();
-        roll.disabled = false;
+      box.forEach((tile) => {
+        if (!tile.classList.contains("flipped")) tile.disabled = true;
+      });
+      checkWin();
+      roll.disabled = false;
+    } else {
+      renderTiles();
     }
-    else {
-        renderTiles();
-    }
-}));
+  })
+);
 
 again.addEventListener("click", () => {
-    die1.replaceChildren();
-    die2.replaceChildren();
+  die1.replaceChildren();
+  die2.replaceChildren();
 
-    roll.disabled = false;
+  roll.disabled = false;
 
-    for (let i = 0; i < 9; i++) {
-        box[i].classList.remove("flipped");
-        box[i].disabled = false;
-    }
+  for (let i = 0; i < 9; i++) {
+    box[i].classList.remove("flipped");
+    box[i].disabled = false;
+  }
 
-    popup.style.display = 'none';
-    overlay.style.display = 'none';
+  popup.style.display = "none";
+  overlay.style.display = "none";
 
-    num = 0;
+  num = 0;
 });
 
 const renderTiles = () => {
-    let useable = [];    // tiles that aren't flipped
-    let clickable = [];  // tiles that can be used to equal num
-    for (let i = 0; i < 9; i++) {
-        if (!box[i].classList.contains("flipped")) {
-            box[i].disabled = true;  // assume tile won't work to start
-            useable.push(box[i]);
-        }
+  let useable = []; // tiles that aren't flipped
+  let clickable = []; // tiles that can be used to equal num
+  for (let i = 0; i < 9; i++) {
+    if (!box[i].classList.contains("flipped")) {
+      box[i].disabled = true; // assume tile won't work to start
+      useable.push(box[i]);
     }
+  }
 
-    // enable viable tiles
-    for (let i = 0; i < useable.length; i++) {
-        let iID = parseInt(useable[i].id);
-        if (iID === num) {
-            useable[i].disabled = false;
-            clickable.push(useable[i]);
+  // enable viable tiles
+  for (let i = 0; i < useable.length; i++) {
+    let iID = parseInt(useable[i].id);
+    if (iID === num) {
+      useable[i].disabled = false;
+      clickable.push(useable[i]);
+    } else {
+      // check for tiles to add to num
+      let activate = numMatch(useable, num);
+      for (let i = 0; i < useable.length; i++) {
+        if (activate.includes(parseInt(useable[i].id))) {
+          useable[i].disabled = false;
+          clickable.push(useable[i]);
         }
-        else {  // check for tiles to add to num
-            let activate = numMatch(useable, num);
-            for (let i = 0; i < useable.length; i++) {
-                if (activate.includes(parseInt(useable[i].id))) {
-                    useable[i].disabled = false;
-                    clickable.push(useable[i]);
-                }
-            }
-        }
+      }
     }
+  }
 
-    if (clickable.length < 1)
-        setTimeout(() => gameOver(), 500);
-}
+  if (clickable.length < 1) setTimeout(() => gameOver(), 500);
+};
 
 const gameOver = () => {
-    overlay.style.display = 'block';
-    end.textContent = 'Game Over';
-    end.style.color = 'var(--red)';
-    popup.style.display = 'block';
-}
+  overlay.style.display = "block";
+  end.textContent = "Game Over";
+  end.style.color = "var(--red)";
+  popup.style.display = "block";
+};
 
 const checkWin = () => {
-    for (let i = 0; i < 9; i++) {
-        if (!box[i].classList.contains("flipped")) {
-            return;
-        }
+  for (let i = 0; i < 9; i++) {
+    if (!box[i].classList.contains("flipped")) {
+      return;
     }
-    win();
-}
+  }
+  win();
+};
 
 const win = () => {
-    overlay.style.display = 'block';
-    end.textContent = 'You Win!';
-    end.style.color = 'var(--green)';
-    popup.style.display = 'block';
-}
+  overlay.style.display = "block";
+  end.textContent = "You Win!";
+  end.style.color = "var(--green)";
+  popup.style.display = "block";
+};
 
 const numMatch = (list, sum) => {
-    let viable = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-        let id = parseInt(list[i].id);
-        if (id === sum) viable.push(id);
-        else if (id < sum) {
-            let tmp = numMatch(list.slice(0, i), sum - id)
-            if (tmp.length > 0) {
-                viable.push(id);
-                viable.push(...tmp);
-            }
-        }
+  let viable = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    let id = parseInt(list[i].id);
+    if (id === sum) viable.push(id);
+    else if (id < sum) {
+      let tmp = numMatch(list.slice(0, i), sum - id);
+      if (tmp.length > 0) {
+        viable.push(id);
+        viable.push(...tmp);
+      }
     }
-    return viable;
-}
+  }
+  return viable;
+};
