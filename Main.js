b74ba8c131be66f5052b1f485295c2f4f1ce5b10
ashtonesawyer <ashtonesawyer@gmail.com>
2024-07-17 12:08:58 -0700

    impliment singe die roll

--- Main.js
+++ Main.js
@@ -2,21 +2,29 @@ const box = document.querySelectorAll(".tile");
 const die1 = document.querySelector("#die1");
 const die2 = document.querySelector("#die2");
 const roll = document.querySelector("#roll");
+const both = document.querySelector("#both");
+const one = document.querySelector("#one");
 const end = document.querySelector("#end");
 const again = document.querySelector("#again");
 const overlay = document.querySelector(".overlay");
+const popup = document.querySelector(".popup");
 
+let choice = false;
 let dice1 = new Dice();
 let dice2 = new Dice();
+let flipped = [];
 let num = 0;
 
-roll.addEventListener("click", () => {
+roll.addEventListener("click", rollBoth);
+both.addEventListener("click", rollBoth);
+
+function rollBoth() {
   shuffle(); // visual for dice roll
 
   // need two instances of Dice() so that they aren't sharing faces in
   // case result1 == result2
-  let result1 = dice1.roll();
-  let result2 = dice2.roll();
+  const result1 = dice1.roll();
+  const result2 = dice2.roll();
 
   console.log(result1 + 1, result2 + 1);
 
@@ -27,6 +35,20 @@ roll.addEventListener("click", () => {
 
   // force tile clicks
   roll.disabled = true;
+  one.disabled = true;
+  both.disabled = true;
+
+  renderTiles();
+}
+
+one.addEventListener("click", () => {
+  die2.style.display = "none";
+
+  num = dice1.roll() + 1;
+  die1.replaceChildren(...dice1.faces[num - 1]);
+
+  one.disabled = true;
+  both.disabled = true;
 
   renderTiles();
 });
@@ -36,13 +58,22 @@ box.forEach((tile) =>
     num -= tile.id;
     tile.classList.add("flipped");
     tile.disabled = true;
+    flipped.push(Number(tile.id));
 
     if (num === 0) {
       box.forEach((tile) => {
         if (!tile.classList.contains("flipped")) tile.disabled = true;
       });
       checkWin();
+
+      if (!choice) checkChoice();
+
       roll.disabled = false;
+      if (choice) {
+        one.disabled = false;
+        both.disabled = false;
+        die2.style.display = "grid";
+      }
     } else {
       renderTiles();
     }
@@ -62,8 +93,14 @@ again.addEventListener("click", () => {
 
   popup.style.display = "none";
   overlay.style.display = "none";
+  one.style.display = "none";
+  both.style.display = "none";
+  roll.style.display = "inline";
+  die2.style.display = "grid";
 
   num = 0;
+  choice = false;
+  flipped = [];
 });
 
 const renderTiles = () => {
@@ -94,14 +131,23 @@ const renderTiles = () => {
     }
   }
 
+  if (!choice) checkChoice();
+
   if (clickable.length < 1) setTimeout(() => gameOver(), 500);
+};
+
+const checkChoice = () => {
+  if (flipped.includes(9) && flipped.includes(8) && flipped.includes(7)) {
+    choice = true;
+    roll.style.display = "none";
+    one.style.display = "inline";
+    both.style.display = "inline";
+  }
 };
 
 const shuffle = () => {
   let faces1 = [];
   let faces2 = [];
-  const dice1 = new Dice();
-  const dice2 = new Dice();
 
   for (let i = 0; i < 5; i++) {
     faces1.push(Math.floor(Math.random() * 6));
